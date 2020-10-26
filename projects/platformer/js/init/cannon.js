5571c9f59462b94e27085432208d10aab0c58cf2
Tavaughn-Mitchell <tavaughnmitchell50@gmail.com>
2020-10-26 23:18:39 +0000

    add portfolio

--- projects/platformer/js/init/cannon.js
+++ projects/platformer/js/init/cannon.js
@@ -25,7 +25,8 @@
         let createCannon = cannon.create;
         ////////////////////////////////////////////////////////////////////////
         // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
-        
+        createCannon("top", 450); 
+createCannon("left", 300, 1000);
         // example: 
         createCannon("top", 450);
         
--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -28,13 +28,20 @@
 
         ////////////////////////////////////////////////////////////////////////
         // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        createPlatform(0, game.world.height - 32, 3, 2);   
         
+        createPlatform(400, 460);           
+createPlatform(300, 200, 0.3)       
+createPlatform(500, 500, 0.3, 10) 
+
+
         /*
          * ground : here, we create a floor. Given the width of of the platform 
          * asset, giving it a scaleX and scaleY of 2 will stretch it across the 
          * bottom of the game.
          */
         createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
+        
 
         // example:
         createPlatform(400, 250);
